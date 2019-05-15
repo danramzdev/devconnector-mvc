7eba003eed78a40488e842a55115f53d97b7c4e7
Daniel Ramirez Mendoza <danramzdev@hotmail.com>
2019-05-15 14:04:23 -0500

    Implementing JWT

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,8 @@
 const { validationResult } = require("express-validator/check");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const User = require("../models/User");
 
@@ -19,7 +21,9 @@ class UsersController {
       let user = await User.findOne({ email });
 
       if (user) {
-        return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
       }
 
       // Get users gravatar
@@ -41,8 +45,23 @@ class UsersController {
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
+
       // Return jsonwebtoken
-      res.send("User registered");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
