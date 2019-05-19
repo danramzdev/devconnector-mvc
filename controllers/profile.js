7a370a365ef4a680ee5c5abc235242b99501f281
Daniel Ramirez Mendoza <danramzdev@hotmail.com>
2019-05-18 21:18:09 -0500

    get current user profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,6 +1,25 @@
+const Profile = require("../models/Profile");
+const User = require("../models/User");
+
 class ProfileController {
-  static index(req, res) {
-    res.send("Profile Route /");
+  static async index(req, res) {
+    try {
+      const profile = await Profile.findOne({ user: req.user.id }).populate(
+        "user",
+        ["name", "avatar"]
+      );
+
+      if (!profile) {
+        return res
+          .status(400)
+          .json({ msg: "There is no profile for this user" });
+      }
+
+      res.json(profile)
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 }
 
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const auth = require("../../utils/middleware/auth");
 
 module.exports = (app, controller) => {
   const router = express.Router();
 
   app.use("/api/profile", router);
 
-  router.get("/", controller.index);
+  router.get("/me", auth, controller.index);
 };
