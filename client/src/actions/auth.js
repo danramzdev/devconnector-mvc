16011b4288f7987bc82f00d7cd4313ed9590858e
Daniel Ramirez Mendoza <danramzdev@hotmail.com>
2019-05-29 15:44:55 -0500

    Logout & Navbar Links

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,7 +5,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from "./types";
 import { setAlert } from "./alert";
 import setAuthToken from "../utils/setAuthToken";
@@ -77,4 +78,9 @@ export const login = (email, password) => async dispatch => {
 
     dispatch({ type: LOGIN_FAIL });
   }
+};
+
+// Logout/Clear profile
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
 };
