9e30781efc14149bd60275ad9d6ddf9d0a9419b1
Daniel Ramirez Mendoza <danramzdev@hotmail.com>
2019-05-31 13:05:44 -0500

    Starting On The Dashboard

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,12 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <a href="/dashboard">
+          <i className="fas fa-user" />{" "}
+          <span className="hide-sm">Dashboard</span>
+        </a>
+      </li>
+      <li>
         <a href="#!" onClick={logout}>
           <i className="fas fa-sign-out-alt" />{" "}
           <span className="hide-sm">Logout</span>
