1564051e500bb962c3c0df9b11b01d1e28590068
Daniel Ramirez Mendoza <danramzdev@hotmail.com>
2019-06-26 11:57:08 -0500

    List experience and education

--- client/src/components/pages/dashboard/Dashboard.js
+++ client/src/components/pages/dashboard/Dashboard.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import DashboardActions from "./DashboardActions";
+import Experience from "./Experience";
+import Education from "./Education";
 import Spinner from "../../layout/Spinner";
 import { getCurrentProfile } from "../../../actions/profile";
 
@@ -27,6 +29,8 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment>
           <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
         </Fragment>
       ) : (
         <Fragment>
@@ -36,58 +40,6 @@ const Dashboard = ({
           </Link>
         </Fragment>
       )}
-
-      <h2 className="my-2">Experience Credentials</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Company</th>
-            <th className="hide-sm">Title</th>
-            <th className="hide-sm">Years</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Tech Guy Web Solutions</td>
-            <td className="hide-sm">Senior Developer</td>
-            <td className="hide-sm">02-03-2009 - 01-02-2014</td>
-            <td>
-              <button className="btn btn-danger">Delete</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Traversy Media</td>
-            <td className="hide-sm">Instructor & Developer</td>
-            <td className="hide-sm">02-03-2015 - Now</td>
-            <td>
-              <button className="btn btn-danger">Delete</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-
-      <h2 className="my-2">Education Credentials</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>School</th>
-            <th className="hide-sm">Degree</th>
-            <th className="hide-sm">Years</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Northern Essex</td>
-            <td className="hide-sm">Associates</td>
-            <td className="hide-sm">02-03-2007 - 01-02-2009</td>
-            <td>
-              <button className="btn btn-danger">Delete</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
 
       <div className="my-2">
         <button className="btn btn-danger">
