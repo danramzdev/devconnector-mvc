a46c9f4d59f19414b129d72dc3e0d256cf7d3f13
Daniel Ramirez Mendoza <danramzdev@hotmail.com>
2019-06-30 13:12:41 -0500

    Addressing The Console Warnings

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -194,7 +194,7 @@ export const deleteEducation = id => async dispatch => {
 export const deleteAccount = () => async dispatch => {
   if (window.confirm("Are tou sure?, This can NOT to be undone!")) {
     try {
-      const res = await axios.delete("/api/profile/");
+      await axios.delete("/api/profile/");
 
       dispatch({ type: CLEAR_PROFILE });
       dispatch({ type: ACCOUNT_DELETED });
