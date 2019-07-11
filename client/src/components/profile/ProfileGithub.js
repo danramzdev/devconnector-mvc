e97122d70732355ee83356bf8b6cb39edb5f39a1
Daniel Ramirez Mendoza <danramzdev@hotmail.com>
2019-07-10 19:39:12 -0500

    Displaying Github Repos

---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import { getGithubRepos } from "../../actions/profile";
+
+const ProfileGithub = ({
+  username,
+  getGithubRepos,
+  profile: { repos, loading }
+}) => {
+  useEffect(() => {
+    getGithubRepos(username);
+  }, [getGithubRepos(username)]);
+
+  return (
+    <div class="profile-github">
+      <h2 class="text-primary my-1">
+        <i class="fab fa-github" /> Github Repos
+      </h2>
+
+      {repos === null && loading
+        ? Spinner
+        : repos.map(repo => (
+            <div class="repo bg-white p-1 my-1" key={repo.id}>
+              <div>
+                <h4>
+                  <a
+                    href={repo.hml_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {repo.name}
+                  </a>
+                </h4>
+                <p>{repo.description}</p>
+              </div>
+              <div>
+                <ul>
+                  <li class="badge badge-primary">
+                    Stars: {repo.stargazers_count}
+                  </li>
+                  <li class="badge badge-dark">
+                    Watchers: {repo.watchers_count}
+                  </li>
+                  <li class="badge badge-light">Forks: {repo.forks_count}</li>
+                </ul>
+              </div>
+            </div>
+          ))}
+    </div>
+  );
+};
+
+ProfileGithub.propTypes = {
+  username: PropTypes.string.isRequired,
+  getGithubRepos: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(
+  mapStateToProps,
+  { getGithubRepos }
+)(ProfileGithub);
