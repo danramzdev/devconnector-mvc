249989ca5166d0dd13e12d8622ce40ac704e4aee
Daniel Ramirez Mendoza <danramzdev@hotmail.com>
2019-07-21 18:56:29 -0500

    Comment Display & Delete

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,6 +6,7 @@ import Spinner from "../layout/Spinner";
 import { getPost } from "../../actions/post";
 import PostItem from "./PostItem";
 import CommentForm from "./CommentForm";
+import CommentItem from "./CommentItem";
 
 const Post = ({ post: { post, loading }, getPost, match }) => {
   useEffect(() => {
@@ -21,6 +22,11 @@ const Post = ({ post: { post, loading }, getPost, match }) => {
       </Link>
       <PostItem post={post} showActions={false}></PostItem>
       <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map(comment => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
